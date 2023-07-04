5b8b795608bdb4d9b201210a1cc3cf651050eada
Linda Brandão <linda.15brandao@gmail.com>
2023-07-04 08:26:55 -0400

    adicionando os sons dos instrumentos em cada quadradinho

---
+++ scripts/main.js
@@ -0,0 +1,35 @@
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        console.log("Elemento não encontrado ou seletor inválido!")
+    }
+}
+
+// acessando dentro do meu documento html o botao da tecla pom
+// acessa o meu documento html, busca o seletor informado e retornar a referencia do elemento que eu quero trabalhar
+//document.querySelector('.tecla_pom').onclick = tocaSom;
+
+// lista de elementos que tem a class como tecla
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let i = 0; i < listaDeTeclas.length; i++) {
+    const tecla = listaDeTeclas[i];
+    const instrumento = tecla.classList[1]; //classList acessa o nome das classes que tem naquela tag
+    const idAudio = `#som_${instrumento}`;
+
+    // quando clicar na minha tecla
+    tecla.onclick = () => tocaSom(idAudio);
+
+    // quando a minha tecla estiver para baixo (pressionada) adicione a class ativa
+     tecla.onkeydown = (evento) => {
+        if (evento.code === "Space" || evento.code === "Enter") {
+            tecla.classList.add('ativa');
+        }
+     }
+
+     // quando a minha tecla estiver para cima (não pressionada) remove a class ativa
+     tecla.onkeyup = () => tecla.classList.remove('ativa');
+}
